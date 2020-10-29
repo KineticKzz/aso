0630ed3dbac6fc02784cf76af6564cb856f7f2c1
root <root@debian207>
2020-10-29 14:37:19 +0100

    Siguiendo el ej3

--- boletin-3/ejercicio3.sh
+++ boletin-3/ejercicio3.sh
@@ -11,15 +11,30 @@ else
 	continue
 fi;}
 funcionMenu(){
-echo "_____________________"
-echo ""
-echo "MENU INFORMACION RED"
-echo "_____________________"
+dialog --nocancel --menu "Menu RED" 40 20 5 /
+1 "Dirección ip interna" /
+2 "Dirección ip pública" /
+3 "MAC" /
+4 "DNS server" /
+5 "Puerta de enlace (gateway)" /
+6 "Nombre del equipo" /
+7 "Salir" 2>fich1
+opcion=$(cat fich1);sleep 1
+rm -r fich1
 }
+#funcionAccionesMenu(){
+#case $1 in
+#1)
+
+#;;
+#2
+#}
 
 funcionComprobacionRoot
+
+dialog --nocancel --inputbox "Hemos encontrado en su equipo las siguientes interfaces, diga de cual quiere la informacion: $(ip a|grep "^[2-9]"|cut -d: -f2)" 0 0 2>fich2
 	
-wget -qO- ifconfig.co/ip
+#wget -qO- ifconfig.co/ip
 	
 	
 	
